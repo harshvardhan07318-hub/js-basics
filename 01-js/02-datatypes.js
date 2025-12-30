3782ad1da322e78d06de0635839d632a0eb193e7
harshvardhan07318-hub <harshvardhan07318@gmail.com>
2025-12-30 07:21:34 +0000

    conversion of datatype

---
+++ 01-js/02-datatypes.js
@@ -0,0 +1,19 @@
+"use strick";//treat all js as newer version 
+//alert (3+3) //use for pop up window in browser not in nde js
+
+//datatypes in js
+let name="harsh"
+let agr=22
+let isloggedin=true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
